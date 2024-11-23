4012fcb506093687627010f8498b925d8ad14385
Cauê Alves <cauealvesb4@gmail.com>
2024-11-23 17:50:13 -0300

    style: Adicionando conteúdo para o estudo de JS

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function calcPorcentagem(x, y) {
-    const conta = (y / x) * 100
-    return conta
+const tt = {
+    
 }
-
-let x = 30
-let y = 15
-let porcentagem = calcPorcentagem(x, y)
-console.log(`${porcentagem}% de ${x} é ${y}`)
